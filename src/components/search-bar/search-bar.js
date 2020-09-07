bcda919b642520ffdb452125fd46772839239466
nourhan adlo <nourhanadlo2@gmail.com>
2020-09-07 11:08:19 +0200

    remove static arr

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -3,9 +3,9 @@
 // import SearchFilter from "../search-filter/search-filter.vue";
 // import ChannelDetails from "../channel-details/channel-details.vue";
 // import VideoDetails from "../video-details/video-details.vue";
-import search_api from'~/local-data-testing/search-api.js';
+// import search_api from'~/local-data-testing/search-api.js';
 // import 'D:\Insta\youtube-app\src\local-data-testing\search-api.js'; 
-// let base_url = "https://www.googleapis.com/youtube/v3/search"
+let base_url = "https://www.googleapis.com/youtube/v3/search"
 export default {
     data() { 
         return ({
@@ -27,11 +27,10 @@ export default {
             // .then(response => response.json())
             // .then((data) => console.log(data))
             // .catch(err => console.log(err))
-            // fetch(base_url +'?part=snippet&key=?????????????&q='+this.searchTxt+'&type='+this.type+'&order='+this.order+'&maxResults=100&')
-            // .then(response => response.json()).then((data) => { if(data !=null)this.list = data ;else this.list = search_api;}
-            //     )
-            console.log("local",search_api)
-                this.list = search_api;
+            fetch(base_url +'?part=snippet&key=AIzaSyAAsxSi3yD_1-PUKptdm1CAidgNtuMcct4&q='+this.searchTxt+'&type='+this.type+'&order='+this.order+'&maxResults=100&')
+            .then(response => response.json()).then((data) => { if(data !=null)this.list = data }
+                )
+            
                
                 this.$root.$emit('sibiling',this.list);
                 
